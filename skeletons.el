547fdb853f10a17335ae3fd2cef6a3eb2b326f07
Cameron Desautels <camdez@gmail.com>
2010-09-27 20:16:39 -0500

    Improved Emacs skeletons, added simple BASH script skeleton

--- skeletons.el
+++ skeletons.el
@@ -4,10 +4,10 @@
 (define-skeleton blog-entry-skeleton
   "Inserts a PyBlosxom-style blog entry skeleton into the current buffer.
 This only makes sense for empty buffers."
-  "Title: "
-  str | "Untitled Entry" \n
+  "Title: " str | "Untitled Entry" \n
   "#postdate " (insert-date-and-time) \n
-  "#tags " ("Enter a tag: " str ",") '(delete-backward-char 1) \n \n
+  "#tags " ("Enter a tag: " str ",") -1 \n
+  \n
   "<p>" _ "</p>")
 
 (define-skeleton c-c++-header-skeleton
@@ -15,34 +15,47 @@ This only makes sense for empty buffers."
   (upcase
    (concat
     "__"
-    (replace-regexp-in-string "\\." "_" (upcase (file-name-nondirectory buffer-file-name)))
+    (replace-regexp-in-string "\\." "_" (file-name-nondirectory buffer-file-name))
     "__"))
-  "#ifndef " str n "#define " str "\n\n" _ "\n\n#endif /*" str "*/")
+  "#ifndef " str \n
+  "#define " str \n \n
+  _ \n \n
+  "#endif /*" str "*/")
 
 (define-skeleton php-header-skeleton
   "Inserts a small PHP skeleton."
   "Description: "
   "<?php" \n \n
-  "// " (file-name-nondirectory buffer-file-name) " - " str \n
+  "// " (file-name-nondirectory buffer-file-name) " - " str | -3 \n
   "//" \n
   "// Author: " user-full-name " <" user-mail-address ">" \n
   "// Date:   " (insert-date-and-time) \n
   "//" \n
-  (copyright user-copyright-holder) \n \n
+  "// " '(copyright user-copyright-holder) \n
+  \n
   _ \n \n
-  "?>" \n
-  )
+  "?>")
+
+(define-skeleton bash-script-skeleton
+  "Inserts a basic hash-bang line for BASH scripts."
+  "Description: "
+  "#!/bin/bash" \n
+  "# " (file-name-nondirectory buffer-file-name) " - " str | -3 \n
+  ?# \n
+  "# Author: " user-full-name " <" user-mail-address ">" \n
+  "# Date:   " (insert-date-and-time) \n
+  \n \n)
 
 (require 'autoinsert)
 
 ;; Autoinsert text into empty files where defined
 (add-hook 'find-file-hooks 'auto-insert)
 
-;; TODO rewrite this -- CARs can be mode-names instead of regexps, which is generally cleaner
 (eval-after-load "autoinsert"
   '(progn
      (add-to-list 'auto-insert-alist '("\\.blog\\'" . blog-entry-skeleton))
-     (add-to-list 'auto-insert-alist '("\\.\\([Hh]\\|hh\\|hpp\\)\\'" . c-c++-header-skeleton))
-     (add-to-list 'auto-insert-alist '("\\.php\\'" . php-header-skeleton))))
+     (add-to-list 'auto-insert-alist '(("\\.\\([Hh]\\|hh\\|hpp\\)\\'" . "C / C++ header") . c-c++-header-skeleton))
+     (add-to-list 'auto-insert-alist '(("\\.php\\'" . "PHP script") . php-header-skeleton))
+     (add-to-list 'auto-insert-alist '(sh-mode . bash-script-skeleton))))
 
 ;;; skeletons.el ends here
