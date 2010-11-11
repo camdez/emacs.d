1ad971c532bca8be82932627ff957d47222dfa70
Cameron Desautels <camdez@gmail.com>
2010-11-11 05:03:44 -0600

    Made .emacs color settings get inherited by new frames

--- theme.el
+++ theme.el
@@ -4,20 +4,19 @@
 (set-foreground-color "white")
 (set-background-color "black")
 
-(eval-after-load 'paren
-  '(progn
-     (set-face-foreground 'show-paren-match-face "red")
-     (set-face-background 'show-paren-match-face "black")))
-
-;; Aquamacs has issues with the basic set-b/f-color calls at init time
-(when (featurep 'aquamacs)
-  (setq default-frame-alist '((background-color . "black")
-                              (foreground-color . "white"))))
-
 ;; Set font for Cocoa Emacs
 (when (featurep 'ns)
   (setq ns-input-font "Droid Sans Mono")
   (setq ns-input-fontsize 13)
   (ns-respond-to-change-font))
+
+; use `current-frame-configuration` to check
+(setq frame-inherited-parameters
+      '(font fontsize background-color foreground-color))
+
+(eval-after-load 'paren
+  '(progn
+     (set-face-foreground 'show-paren-match-face "red")
+     (set-face-background 'show-paren-match-face "black")))
 
 ;;; theme.el ends here
