783dceb4c1c8e73951a766119af2f5c91c843e84
Cameron Desautels <camdez@gmail.com>
2012-04-09 19:20:17 -0500

    Emacs: make `l' go back in help-mode, consistent with info-mode

--- keys.el
+++ keys.el
@@ -34,6 +34,8 @@
 
 (global-set-key "\C-ca" 'org-agenda)
 
+(define-key help-mode-map "l" 'help-go-back)   ; consistent with info-mode
+
 (set-register ?e (cons 'file user-init-file))  ; quickly jump to init.el (".emacs") with C-x r j e
 (set-register ?s (cons 'file "~/.bashrc"))     ; quickly jump to .bashrc with C-x r j s
 
