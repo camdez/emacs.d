92a91128f8ac225bcd9cac6a2442a7d374aec0b5
Cameron Desautels <camdez@gmail.com>
2015-04-06 15:56:14 -0500

    core/config.el: clean-up

--- core/config.el
+++ core/config.el
@@ -1,51 +1,71 @@
 ;;; config.el --- various general settings
 ;;; Author: Cameron Desautels <camdez@gmail.com>
 
-(setq user-full-name "Cameron Desautels")
-(setq user-mail-address "camdez@gmail.com")
-(setq user-copyright-holder "Cameron Desautels")           ;my creation
+(setq user-full-name "Cameron Desautels"
+      user-mail-address "camdez@gmail.com"
+      user-copyright-holder "Cameron Desautels" ; my creation
 
-(setq echo-keystrokes 0.02)
-(setq inhibit-startup-message t)
+      kill-ring-max 20                          ; don't save too many kills
+      history-length 250
 
-(setq visible-bell t)
-(blink-cursor-mode -1)                  ; make the bloody cursor stop blinking
-(setq x-stretch-cursor t)               ; make the cursor wide over spaces, etc.
-(setq kill-ring-max 20)                 ; don't save too many kills (I don't use many)
-(setq history-length 250)
-(setq require-final-newline t)          ; always terminate last line in file
-(setq default-major-mode 'text-mode)    ; default mode is text mode
-(setq default-indicate-empty-lines t)   ; show which lines at the end of the buffer are blank via the gutter
-(setq view-read-only t)                 ; use potentially-handy mode instead of just beeping all the time
+      inhibit-startup-message t
+      default-major-mode 'text-mode             ; default mode is text mode
+      require-final-newline t                   ; always terminate last line in file
+      view-read-only t                          ; use handy mode instead of just beeping all the time
+      vc-follow-symlinks t
 
-(transient-mark-mode 0)                 ; no way, dude
-(cua-mode 0)                            ; Aquamacs turns this crap on (messes with transient-mark-mode too)
-(setq scroll-preserve-screen-position t ; when scrolling, keep point in the same position on screen
-      isearch-allow-scroll t)           ; scrolling shouldn't cancel search
+      visible-bell t
+      x-stretch-cursor t                        ; make the cursor wide over tabs, etc.
+      blink-matching-paren nil                  ; not needed since I'm highlighting the matches
+      frame-title-format "Emacs: %b %+%+ %f"
+      echo-keystrokes 0.02
+      isearch-allow-scroll t                    ; scrolling shouldn't cancel search
+      scroll-preserve-screen-position t         ; when scrolling, keep point in the same position on screen
+      default-indicate-empty-lines t
 
-(global-font-lock-mode t)               ; use syntax highlighting
-(column-number-mode)
-(show-paren-mode t)                     ; highlight matching parentheses
-(setq blink-matching-paren nil)         ; not needed since I'm highlighting the matches
+      version-control t                         ; keep numbered back-ups
+      delete-old-versions t
+      kept-new-versions 6
+      kept-old-versions 2)
 
-(setq-default truncate-lines t)
+(setq-default tab-width 2                       ; display tabs as being two spaces wide
+              indent-tabs-mode nil              ; use spaces (not tabs) for indenting
+              show-trailing-whitespace t
+              truncate-lines t)                 ; don't visually wrap long lines
 
-(setq frame-title-format "Emacs: %b %+%+ %f")
-(setq ange-ftp-ftp-program-name "ftp")
+;; Only under Aquamacs Emacs
+(when (featurep 'aquamacs)
+  (setq mac-command-modifier 'meta
+        mac-allow-anti-aliasing t
+        x-select-enable-clipboard t))
 
-(setq vc-follow-symlinks t)
+;; Only under Cocoa Emacs
+(when (featurep 'ns)
+  (setq ns-command-modifier 'meta
+        trash-directory "~/.Trash"
+        delete-by-moving-to-trash t
+        dired-use-ls-dired nil))
 
-(setq-default tab-width 2)              ; display tabs as being two spaces wide
-(setq-default indent-tabs-mode nil)     ; use spaces (not tabs) for indenting
-(setq-default show-trailing-whitespace t)
+;; Only under the Mac port
+(when (featurep 'mac)
+  (setq mac-option-modifier 'meta))
 
-(global-auto-revert-mode 1)             ; automatically reload files which are externally modified
-(auto-image-file-mode 1)                ; open images as images
-(auto-compression-mode 1)               ; automagically explore compressed files when visited
-(menu-bar-mode -1)                      ; hide the menu bar
-(when window-system
-  (tool-bar-mode -1)                    ; hide the tool bar
-  (scroll-bar-mode -1))                 ; put the scroll bar on the right where it should be
+(blink-cursor-mode       -1)     ; make the bloody cursor stop blinking
+(transient-mark-mode     -1)     ; no way, dude
+(cua-mode                -1)     ; Aquamacs turns this crap on (messes with transient-mark-mode too)
+(global-font-lock-mode   +1)     ; use syntax highlighting
+(column-number-mode      +1)
+(show-paren-mode         +1)     ; highlight matching parentheses
+(global-auto-revert-mode +1)     ; automatically reload files which are externally modified
+(auto-image-file-mode    +1)     ; open images as images
+(auto-compression-mode   +1)     ; automagically explore compressed files when visited
+(ido-mode                +1)     ; use awesome buffer switching mode
+
+(if window-system
+    (progn
+      (tool-bar-mode -1)         ; hide the tool bar
+      (scroll-bar-mode -1))      ; put the scroll bar on the right
+  (menu-bar-mode -1))            ; hide the menu bar
 
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 (add-hook 'after-save-hook 'executable-make-buffer-file-executable-if-script-p)
@@ -64,41 +84,11 @@
 (add-hook 'comint-output-filter-functions
           'comint-watch-for-password-prompt)
 
-;; Use awesome buffer switching mode
-;; (if (> emacs-major-version 21)
-;;     (iswitchb-mode 1)
-;;   (iswitchb-default-keybindings))
-(ido-mode 1)
-
-;; Keep numbered backups
-(setq delete-old-versions t
-      kept-new-versions 6
-      kept-old-versions 2
-      version-control t)       ; use versioned backups
-
-;; Only under Aquamacs Emacs
-(when (featurep 'aquamacs)
-  (setq mac-command-modifier 'meta)
-  (setq mac-allow-anti-aliasing t)
-  (setq x-select-enable-clipboard t))
-
-;; Only under Cocoa Emacs
-(when (featurep 'ns)
-  (setq ns-command-modifier 'meta)
-  (setq trash-directory "~/.Trash")
-  (setq delete-by-moving-to-trash t)
-  (setq dired-use-ls-dired nil))
-
-;; Only under the Mac port
-(when (featurep 'mac)
-  (setq mac-option-modifier 'meta))
-
 ;; Advice
-(defadvice kill-some-buffers (around kill-some-buffers-y-or-n first (&optional list))
+(defadvice kill-some-buffers (around kill-some-buffers-y-or-n first)
   "When running `kill-some-buffers' use 'y' and 'n' for response,
 regardless of if 'yes' or 'no' are generally preferred."
-  (flet ((yes-or-no-p (args)
-           (y-or-n-p args)))
+  (flet ((yes-or-no-p (args) (y-or-n-p args)))
     ad-do-it))
 (ad-activate 'kill-some-buffers)
 
