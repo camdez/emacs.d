camdez/emacs.d

commit 4a6e8ce2578b46a012ff6825ad54513e0f077745

Stylistic improvements, move away from deprecated variables

core/config.el
@@ -9,7 +9,7 @@
       history-length 250
 
       inhibit-startup-message t
-      default-major-mode 'text-mode             ; default mode is text mode
+      major-mode 'text-mode                     ; default mode is text mode
       require-final-newline t                   ; always terminate last line in file
       view-read-only t                          ; use handy mode instead of just beeping all the time
       vc-follow-symlinks t
@@ -21,7 +21,7 @@
       echo-keystrokes 0.02
       isearch-allow-scroll t                    ; scrolling shouldn't cancel search
       scroll-preserve-screen-position t         ; when scrolling, keep point in the same position on screen
-      default-indicate-empty-lines t
+      indicate-empty-lines t
 
       version-control t                         ; keep numbered back-ups
       delete-old-versions t

core/commands.el
@@ -356,7 +356,7 @@ if at the beginning of a line."
   (interactive)
   (let ((cc (current-column)))
     (when (= cc 0)
-      (previous-line 1)
+      (forward-line -1)
       (end-of-line)
       (setq cc (current-column)))
     (newline)

core/keys.el
@@ -1,10 +1,10 @@
 ;;; keys.el --- Emacs keybindings
 ;;; Author: Cameron Desautels <camdez@gmail.com>
 
-(mapc '(lambda (binding)
-         (let ((key (car binding))
-               (command (cadr binding)))
-           (global-set-key (kbd key) command)))
+(mapc #'(lambda (binding)
+          (let ((key (car binding))
+                (command (cadr binding)))
+            (global-set-key (kbd key) command)))
       '(("<del>"     delete-char)
         ("<f11>"     compile)
         ("<f12>"     recompile)

commit ed0ce4c3445d803bc7a3fc993cee0c3872f6af28

Patch up defaults broken in 4a6e8ce2

core/config.el
@@ -9,7 +9,6 @@
       history-length 250
 
       inhibit-startup-message t
-      major-mode 'text-mode                     ; default mode is text mode
       require-final-newline t                   ; always terminate last line in file
       view-read-only t                          ; use handy mode instead of just beeping all the time
       vc-follow-symlinks t
@@ -21,16 +20,17 @@
       echo-keystrokes 0.02
       isearch-allow-scroll t                    ; scrolling shouldn't cancel search
       scroll-preserve-screen-position t         ; when scrolling, keep point in the same position on screen
-      indicate-empty-lines t
 
       version-control t                         ; keep numbered back-ups
       delete-old-versions t
       kept-new-versions 6
       kept-old-versions 2)
 
-(setq-default tab-width 2                       ; display tabs as being two spaces wide
+(setq-default major-mode 'text-mode             ; default mode is text mode
+              tab-width 2                       ; display tabs as being two spaces wide
               indent-tabs-mode nil              ; use spaces (not tabs) for indenting
               show-trailing-whitespace t
+              indicate-empty-lines t
               truncate-lines t)                 ; don't visually wrap long lines
 
 ;; Only under Aquamacs Emacs