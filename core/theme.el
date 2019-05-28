41573f1ce9a613ddcace945615108ba20919919c
Cameron Desautels <camdez@gmail.com>
2019-05-28 13:32:25 -0500

    Highlight a few key faces
    (Been highlighting these for ages in my experiments / custom files.)

--- core/theme.el
+++ core/theme.el
@@ -5,6 +5,7 @@
 (set-foreground-color "white")
 (set-background-color "black")
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; use `current-frame-configuration' to check
 (setq frame-inherited-parameters
       '(font fontsize background-color foreground-color))
@@ -13,12 +14,18 @@
 (load-theme 'monokai t)
 
 ;;; Tweak additional faces
+(set-face-background 'region "gray40")
+(set-face-background 'trailing-whitespace "gray19")
+
+(eval-after-load 'eldoc
+  '(set-face-underline 'eldoc-highlight-function-argument "red"))
+
 (eval-after-load 'paren
   '(progn
      (set-face-foreground 'show-paren-match "black")
-     (set-face-background 'show-paren-match "red")))
-
-(set-face-background 'trailing-whitespace "gray19")
+     (set-face-background 'show-paren-match "red")
+     (set-face-background 'show-paren-match-expression "gray35")
+     (set-face-attribute 'show-paren-match-expression nil :inherit nil)))
 
 (eval-after-load 'diff-mode
   '(progn
